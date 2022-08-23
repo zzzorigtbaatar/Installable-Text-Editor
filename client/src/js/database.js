25df47e5733824fdaffcee7f4f930b9711cbd1f4
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-22 22:14:44 -0700

    add get and put routes to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ content: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  if (result[0]) {
+    return result[result.length - 1].content;
+  }
+  return false;
+};
 
 initdb();
